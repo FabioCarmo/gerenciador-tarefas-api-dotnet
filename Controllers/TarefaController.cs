f05dc29e37bfc22ad5eddb0cd23e5255ce4abc07
fabio <fcarmouk13@gmail.com>
2025-12-02 23:06:38 -0300

    Add: Adicionado novos metodos

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -17,10 +17,10 @@ namespace GerenciadorTarefas.Controllers
             _context = context;
         }
 
-        // Obter o Id no DB
+        // Obter a tarefa por Id no DB
         [HttpGet("{id}")]
         public IActionResult ObterId(int id) {
-            var tarefa = _context.Tarefas.Find(id);
+            var tarefa = _context.Tarefas.Find(id); // Procura o valor do parametro no banco
 
             if (tarefa == null) {
                 return NotFound();
@@ -29,7 +29,20 @@ namespace GerenciadorTarefas.Controllers
             return Ok(tarefa);
         }
 
-        // Criar uma nova tarefa para inserir no DB
+        // Obter tarefa pelo titulo
+        [HttpGet("ObterPorTitulo")]
+        public IActionResult ObterTitulo(string titulo) {
+            // Busca o valor do parametro passado no campo titulo do db
+            var tarefadb = _context.Tarefas.Where(x => x.Titulo == titulo);
+
+            if (tarefadb == null) {
+                return NotFound();
+            }
+
+            return Ok(tarefadb);
+        }
+
+        // Criar uma nova tarefa para inserir no DB passando como parametro o JSON tarefa
         [HttpPost]
         public IActionResult Criar(Tarefa tarefa) {
 
@@ -40,6 +53,48 @@ namespace GerenciadorTarefas.Controllers
             _context.Add(tarefa);
             _context.SaveChanges();
             return Ok(tarefa);
-        }    
+        } 
+
+        // Atualizar nova tarefa no DB 
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Tarefa tarefa) {
+            var tarefadb = _context.Tarefas.Find(id);
+
+            if (tarefadb == null) {
+                return NotFound();
+            }
+
+            if (tarefa.Data == DateTime.MinValue)
+            {
+                return BadRequest(new {erro = "A data da tarefa não pode ser vazia !"});
+            }
+
+            // Atribui novos valores a propriedade do banco
+            tarefadb.Descricao = tarefa.Descricao;
+            tarefadb.Titulo = tarefa.Titulo;
+            tarefadb.Data = tarefa.Data;
+            tarefadb.Status = tarefa.Status;
+
+            // Atualiza e Salva no DB
+            _context.Update(tarefadb);
+            _context.SaveChanges();
+
+            return Ok(tarefa);
+        }
+
+        // Apaga a tarefa no DB passando como parametro o id da tarefa
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id) {
+            var tarefadb = _context.Tarefas.Find(id);
+
+            if (tarefadb == null) {
+                return NotFound();
+            }
+
+            // Remover e Salvar
+            _context.Tarefas.Remove(tarefadb);
+            _context.SaveChanges();
+            return NoContent();
+        }   
     }
 }
